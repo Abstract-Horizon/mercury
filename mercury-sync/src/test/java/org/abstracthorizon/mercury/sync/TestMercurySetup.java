4ee230e1240a7890f1a5c1c6a7934ae32f92a37d
Daniel Sendula <natdan@users.noreply.github.com>
2020-08-09 16:27:41 +0100

    Replaced tabs with spaces

--- mercury-sync/src/test/java/org/abstracthorizon/mercury/sync/TestMercurySetup.java
+++ mercury-sync/src/test/java/org/abstracthorizon/mercury/sync/TestMercurySetup.java
@@ -17,209 +17,209 @@ import org.abstracthorizon.mercury.smtp.logging.SMTPSPAMAccessLogConnectionHandl
 
 public class TestMercurySetup {
 
-	private MultiThreadServerSocketService multiThreadServerSocketService;
-	private LoggingConnectionHandler loggingConnectionHandler;
-	private SMTPFilterCommandFactory smtpFilterCommandFactory;
-	private SMTPSPAMAccessLogConnectionHandler smtpspamAccessLogConnectionHandler;
-	private int port = -1;
-	private File propertiesFile;
-	private File keystoreFile;
-	private MercuryDirSetup mercuryDirSetup;
-	private File tempDir;
-	private File configDir;
+    private MultiThreadServerSocketService multiThreadServerSocketService;
+    private LoggingConnectionHandler loggingConnectionHandler;
+    private SMTPFilterCommandFactory smtpFilterCommandFactory;
+    private SMTPSPAMAccessLogConnectionHandler smtpspamAccessLogConnectionHandler;
+    private int port = -1;
+    private File propertiesFile;
+    private File keystoreFile;
+    private MercuryDirSetup mercuryDirSetup;
+    private File tempDir;
+    private File configDir;
 
-	public TestMercurySetup() {
-		smtpspamAccessLogConnectionHandler = new SMTPSPAMAccessLogConnectionHandler();
-	}
+    public TestMercurySetup() {
+        smtpspamAccessLogConnectionHandler = new SMTPSPAMAccessLogConnectionHandler();
+    }
 
-	public void setupDir() throws IOException {
-		mercuryDirSetup = new MercuryDirSetup();
-		tempDir = mercuryDirSetup.create();
-		mercuryDirSetup.getMailboxes().mkdir();
-		System.out.println(tempDir.getAbsolutePath());
+    public void setupDir() throws IOException {
+        mercuryDirSetup = new MercuryDirSetup();
+        tempDir = mercuryDirSetup.create();
+        mercuryDirSetup.getMailboxes().mkdir();
+        System.out.println(tempDir.getAbsolutePath());
 
-		createConfigs();
+        createConfigs();
 
-	}
+    }
 
-	public void createConfigs() throws IOException {
-		configDir = new File(tempDir, "config");
-		if (!configDir.mkdir()) {
-			throw new IOException("Cannot create " + configDir.getAbsolutePath());
-		}
+    public void createConfigs() throws IOException {
+        configDir = new File(tempDir, "config");
+        if (!configDir.mkdir()) {
+            throw new IOException("Cannot create " + configDir.getAbsolutePath());
+        }
 
-		propertiesFile = new File(configDir, "accounts.properties");
-		TestUtils.writeFile(propertiesFile, "");
+        propertiesFile = new File(configDir, "accounts.properties");
+        TestUtils.writeFile(propertiesFile, "");
 
-		keystoreFile = new File(configDir, "accounts.keystore");
-		try (InputStream keystoreStream = getClass().getClassLoader().getResourceAsStream("setup/accounts.keystore")) {
-			TestUtils.writeFile(keystoreFile, keystoreStream);
-		}
+        keystoreFile = new File(configDir, "accounts.keystore");
+        try (InputStream keystoreStream = getClass().getClassLoader().getResourceAsStream("setup/accounts.keystore")) {
+            TestUtils.writeFile(keystoreFile, keystoreStream);
+        }
 
-	}
+    }
 
-	public TestMercurySetup(File propertiesFile, File keystoreFile) {
-		this.propertiesFile = propertiesFile;
-		this.keystoreFile = keystoreFile;
-	}
+    public TestMercurySetup(File propertiesFile, File keystoreFile) {
+        this.propertiesFile = propertiesFile;
+        this.keystoreFile = keystoreFile;
+    }
 
-	public void create() throws IOException {
-		ServerSocket socket = new ServerSocket(0);
-		port = socket.getLocalPort();
-		socket.close();
+    public void create() throws IOException {
+        ServerSocket socket = new ServerSocket(0);
+        port = socket.getLocalPort();
+        socket.close();
 
-		MaildirKeystoreStorageManager storageManager = new MaildirKeystoreStorageManager();
-		storageManager.setPropertiesFile(propertiesFile);
-		storageManager.setKeyStoreFile(keystoreFile);
-		storageManager.setMailboxesPath(getMailboxes());
-		storageManager.setKeyStorePassword("password1234");
-		storageManager.init();
+        MaildirKeystoreStorageManager storageManager = new MaildirKeystoreStorageManager();
+        storageManager.setPropertiesFile(propertiesFile);
+        storageManager.setKeyStoreFile(keystoreFile);
+        storageManager.setMailboxesPath(getMailboxes());
+        storageManager.setKeyStorePassword("password1234");
+        storageManager.init();
 
-		multiThreadServerSocketService = new MultiThreadServerSocketService();
+        multiThreadServerSocketService = new MultiThreadServerSocketService();
 
-		multiThreadServerSocketService.setName("smtp");
-		multiThreadServerSocketService.setPort(port);
-		multiThreadServerSocketService.setAddress("localhost");
-		multiThreadServerSocketService.setServerSocketTimeout(1000);
-		multiThreadServerSocketService.setNewSocketTimeout(60000);
+        multiThreadServerSocketService.setName("smtp");
+        multiThreadServerSocketService.setPort(port);
+        multiThreadServerSocketService.setAddress("localhost");
+        multiThreadServerSocketService.setServerSocketTimeout(1000);
+        multiThreadServerSocketService.setNewSocketTimeout(60000);
 
-		loggingConnectionHandler = new LoggingConnectionHandler();
-		multiThreadServerSocketService.setConnectionHandler(loggingConnectionHandler);
+        loggingConnectionHandler = new LoggingConnectionHandler();
+        multiThreadServerSocketService.setConnectionHandler(loggingConnectionHandler);
 
-		SMTPConnectionHandler smtpConnectionHandler = new SMTPConnectionHandler();
-		smtpConnectionHandler.setStorageManager(storageManager);
-		loggingConnectionHandler.setConnectionHandler(smtpConnectionHandler);
+        SMTPConnectionHandler smtpConnectionHandler = new SMTPConnectionHandler();
+        smtpConnectionHandler.setStorageManager(storageManager);
+        loggingConnectionHandler.setConnectionHandler(smtpConnectionHandler);
 
-		smtpFilterCommandFactory = new SMTPFilterCommandFactory();
-		smtpFilterCommandFactory.setFilters(Arrays.<Filter>asList(new FindStorageFilter()));
-		smtpFilterCommandFactory.init();
-		smtpFilterCommandFactory.setInactivityTimeout(60000);
-		smtpConnectionHandler.setConnectionHandler(smtpFilterCommandFactory);
+        smtpFilterCommandFactory = new SMTPFilterCommandFactory();
+        smtpFilterCommandFactory.setFilters(Arrays.<Filter>asList(new FindStorageFilter()));
+        smtpFilterCommandFactory.init();
+        smtpFilterCommandFactory.setInactivityTimeout(60000);
+        smtpConnectionHandler.setConnectionHandler(smtpFilterCommandFactory);
 
-		multiThreadServerSocketService.create();
-		multiThreadServerSocketService.start();
+        multiThreadServerSocketService.create();
+        multiThreadServerSocketService.start();
 
-		System.out.println("Started multiThreadServerSocketService on at " + multiThreadServerSocketService.getAddress()
-				+ " on port " + multiThreadServerSocketService.getPort());
-	}
+        System.out.println("Started multiThreadServerSocketService on at " + multiThreadServerSocketService.getAddress()
+                + " on port " + multiThreadServerSocketService.getPort());
+    }
 
-	public void setupMailBox(String address) throws IOException {
+    public void setupMailBox(String address) throws IOException {
 
-		String[] split = address.split("@");
+        String[] split = address.split("@");
 
-		if (split.length < 2) {
-			throw new IOException("Invalid mailbox address: " + address);
-		}
-		String name = split[0];
-		String domain = split[1];
+        if (split.length < 2) {
+            throw new IOException("Invalid mailbox address: " + address);
+        }
+        String name = split[0];
+        String domain = split[1];
 
-		File mailbox = new File(getMailboxes(), domain);
-		if (!mailbox.mkdir()) {
-			throw new IOException("Cannot create " + mailbox.getAbsolutePath());
-		}
-		File mail = new File(mailbox, name);
-		if (!mail.mkdir()) {
-			throw new IOException("Cannot create " + mail.getAbsolutePath());
-		}
-		File inbox = new File(mail, ".inbox");
-		if (!inbox.mkdir()) {
-			throw new IOException("Cannot create " + inbox.getAbsolutePath());
-		}
+        File mailbox = new File(getMailboxes(), domain);
+        if (!mailbox.mkdir()) {
+            throw new IOException("Cannot create " + mailbox.getAbsolutePath());
+        }
+        File mail = new File(mailbox, name);
+        if (!mail.mkdir()) {
+            throw new IOException("Cannot create " + mail.getAbsolutePath());
+        }
+        File inbox = new File(mail, ".inbox");
+        if (!inbox.mkdir()) {
+            throw new IOException("Cannot create " + inbox.getAbsolutePath());
+        }
 
-		File cur1 = new File(inbox, "cur");
-		if (!cur1.mkdir()) {
-			throw new IOException("Cannot create " + cur1.getAbsolutePath());
-		}
-		File new1 = new File(inbox, "new");
-		if (!new1.mkdir()) {
-			throw new IOException("Cannot create " + new1.getAbsolutePath());
-		}
-		File tmp1 = new File(inbox, "tmp");
-		if (!tmp1.mkdir()) {
-			throw new IOException("Cannot create " + tmp1.getAbsolutePath());
-		}
+        File cur1 = new File(inbox, "cur");
+        if (!cur1.mkdir()) {
+            throw new IOException("Cannot create " + cur1.getAbsolutePath());
+        }
+        File new1 = new File(inbox, "new");
+        if (!new1.mkdir()) {
+            throw new IOException("Cannot create " + new1.getAbsolutePath());
+        }
+        File tmp1 = new File(inbox, "tmp");
+        if (!tmp1.mkdir()) {
+            throw new IOException("Cannot create " + tmp1.getAbsolutePath());
+        }
 
-		StringBuilder properties = new StringBuilder();
+        StringBuilder properties = new StringBuilder();
 
-		if (getPropertiesFile().exists()) {
-			String loadFile = TestUtils.loadFile(getPropertiesFile());
-			properties.append(loadFile);
-		}
+        if (getPropertiesFile().exists()) {
+            String loadFile = TestUtils.loadFile(getPropertiesFile());
+            properties.append(loadFile);
+        }
 
-		properties.append("-@" + domain + "=\n");
-		properties.append("-" + name + "@" + domain + "=S\\=\n");
-		TestUtils.writeFile(getPropertiesFile(), properties.toString());
+        properties.append("-@" + domain + "=\n");
+        properties.append("-" + name + "@" + domain + "=S\\=\n");
+        TestUtils.writeFile(getPropertiesFile(), properties.toString());
 
-		System.out.println(getPropertiesFile().getAbsolutePath());
-	}
+        System.out.println(getPropertiesFile().getAbsolutePath());
+    }
 
-	public void cleanup() {
-		multiThreadServerSocketService.stop();
+    public void cleanup() {
+        multiThreadServerSocketService.stop();
 
-		multiThreadServerSocketService.destroy();
-		smtpFilterCommandFactory.cleanup();
+        multiThreadServerSocketService.destroy();
+        smtpFilterCommandFactory.cleanup();
 
-	}
+    }
 
-	public MultiThreadServerSocketService getMultiThreadServerSocketService() {
-		return multiThreadServerSocketService;
-	}
+    public MultiThreadServerSocketService getMultiThreadServerSocketService() {
+        return multiThreadServerSocketService;
+    }
 
-	public void setMultiThreadServerSocketService(MultiThreadServerSocketService multiThreadServerSocketService) {
-		this.multiThreadServerSocketService = multiThreadServerSocketService;
-	}
+    public void setMultiThreadServerSocketService(MultiThreadServerSocketService multiThreadServerSocketService) {
+        this.multiThreadServerSocketService = multiThreadServerSocketService;
+    }
 
-	public LoggingConnectionHandler getLoggingConnectionHandler() {
-		return loggingConnectionHandler;
-	}
+    public LoggingConnectionHandler getLoggingConnectionHandler() {
+        return loggingConnectionHandler;
+    }
 
-	public void setLoggingConnectionHandler(LoggingConnectionHandler loggingConnectionHandler) {
-		this.loggingConnectionHandler = loggingConnectionHandler;
-	}
+    public void setLoggingConnectionHandler(LoggingConnectionHandler loggingConnectionHandler) {
+        this.loggingConnectionHandler = loggingConnectionHandler;
+    }
 
-	public SMTPFilterCommandFactory getSmtpFilterCommandFactory() {
-		return smtpFilterCommandFactory;
-	}
+    public SMTPFilterCommandFactory getSmtpFilterCommandFactory() {
+        return smtpFilterCommandFactory;
+    }
 
-	public void setSmtpFilterCommandFactory(SMTPFilterCommandFactory smtpFilterCommandFactory) {
-		this.smtpFilterCommandFactory = smtpFilterCommandFactory;
-	}
+    public void setSmtpFilterCommandFactory(SMTPFilterCommandFactory smtpFilterCommandFactory) {
+        this.smtpFilterCommandFactory = smtpFilterCommandFactory;
+    }
 
-	public SMTPSPAMAccessLogConnectionHandler getSmtpspamAccessLogConnectionHandler() {
-		return smtpspamAccessLogConnectionHandler;
-	}
+    public SMTPSPAMAccessLogConnectionHandler getSmtpspamAccessLogConnectionHandler() {
+        return smtpspamAccessLogConnectionHandler;
+    }
 
-	public void setSmtpspamAccessLogConnectionHandler(
-			SMTPSPAMAccessLogConnectionHandler smtpspamAccessLogConnectionHandler) {
-		this.smtpspamAccessLogConnectionHandler = smtpspamAccessLogConnectionHandler;
-	}
+    public void setSmtpspamAccessLogConnectionHandler(
+            SMTPSPAMAccessLogConnectionHandler smtpspamAccessLogConnectionHandler) {
+        this.smtpspamAccessLogConnectionHandler = smtpspamAccessLogConnectionHandler;
+    }
 
-	public int getPort() {
-		return port;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	public MercuryDirSetup getMercuryDirSetup() {
-		return mercuryDirSetup;
-	}
+    public MercuryDirSetup getMercuryDirSetup() {
+        return mercuryDirSetup;
+    }
 
-	public void setMercuryDirSetup(MercuryDirSetup mercuryDirSetup) {
-		this.mercuryDirSetup = mercuryDirSetup;
-	}
+    public void setMercuryDirSetup(MercuryDirSetup mercuryDirSetup) {
+        this.mercuryDirSetup = mercuryDirSetup;
+    }
 
-	public File getMailboxes() {
-		return mercuryDirSetup.getMailboxes();
-	}
+    public File getMailboxes() {
+        return mercuryDirSetup.getMailboxes();
+    }
 
-	public File getPropertiesFile() {
-		return propertiesFile;
-	}
+    public File getPropertiesFile() {
+        return propertiesFile;
+    }
 
-	public void setPropertiesFile(File propertiesFile) {
-		this.propertiesFile = propertiesFile;
-	}
+    public void setPropertiesFile(File propertiesFile) {
+        this.propertiesFile = propertiesFile;
+    }
 
 }
