3ec2b6494fb33d356221c359bb8cc1880baafe50
Daniel Sendula <natdan@users.noreply.github.com>
2020-08-10 08:33:40 +0100

    Removed tabs

--- mercury-sync/src/test/java/org/abstracthorizon/mercury/sync/TestSMTP.java
+++ mercury-sync/src/test/java/org/abstracthorizon/mercury/sync/TestSMTP.java
@@ -31,73 +31,73 @@ import org.junit.Test;
 
 public class TestSMTP {
 
-	private static final String DESTINATION_MAILBOX = "test@test.com";
+    private static final String DESTINATION_MAILBOX = "test@test.com";
 
-	private TestMercurySetup testMercurySetup1;
-	private TestMercurySetup testMercurySetup2;
+    private TestMercurySetup testMercurySetup1;
+    private TestMercurySetup testMercurySetup2;
 
-	@Before
-	public void setup() throws IOException {
-		testMercurySetup1 = new TestMercurySetup();
+    @Before
+    public void setup() throws IOException {
+        testMercurySetup1 = new TestMercurySetup();
 
-		testMercurySetup1.setupDir();
-		testMercurySetup1.setupMailBox(DESTINATION_MAILBOX);
+        testMercurySetup1.setupDir();
+        testMercurySetup1.setupMailBox(DESTINATION_MAILBOX);
 
-		testMercurySetup1.create();
+        testMercurySetup1.create();
 
 
-		testMercurySetup2 = new TestMercurySetup();
+        testMercurySetup2 = new TestMercurySetup();
 
-		testMercurySetup2.setupDir();
-		testMercurySetup2.setupMailBox(DESTINATION_MAILBOX);
+        testMercurySetup2.setupDir();
+        testMercurySetup2.setupMailBox(DESTINATION_MAILBOX);
 
-		testMercurySetup2.create();
-	}
+        testMercurySetup2.create();
+    }
 
-	@After
-	public void tearDown() {
-		testMercurySetup1.cleanup();
-		testMercurySetup2.cleanup();
-	}
+    @After
+    public void tearDown() {
+        testMercurySetup1.cleanup();
+        testMercurySetup2.cleanup();
+    }
 
-	@Test
-	public void testSMTPmail() throws IOException {
-		assertEquals(0, listAllMailboxFiles(testMercurySetup1.getMailboxes(), DESTINATION_MAILBOX).size());
-		sendMail(DESTINATION_MAILBOX, testMercurySetup1.getPort());
-		assertEquals(1, listAllMailboxFiles(testMercurySetup1.getMailboxes(), DESTINATION_MAILBOX).size());
+    @Test
+    public void testSMTPmail() throws IOException {
+        assertEquals(0, listAllMailboxFiles(testMercurySetup1.getMailboxes(), DESTINATION_MAILBOX).size());
+        sendMail(DESTINATION_MAILBOX, testMercurySetup1.getPort());
+        assertEquals(1, listAllMailboxFiles(testMercurySetup1.getMailboxes(), DESTINATION_MAILBOX).size());
 
-		assertEquals(0, listAllMailboxFiles(testMercurySetup2.getMailboxes(), DESTINATION_MAILBOX).size());
-		sendMail(DESTINATION_MAILBOX, testMercurySetup2.getPort());
-		assertEquals(1, listAllMailboxFiles(testMercurySetup2.getMailboxes(), DESTINATION_MAILBOX).size());
-	}
+        assertEquals(0, listAllMailboxFiles(testMercurySetup2.getMailboxes(), DESTINATION_MAILBOX).size());
+        sendMail(DESTINATION_MAILBOX, testMercurySetup2.getPort());
+        assertEquals(1, listAllMailboxFiles(testMercurySetup2.getMailboxes(), DESTINATION_MAILBOX).size());
+    }
 
-	public void sendMail(String address, int port) throws IOException {
-		String to = address;
-		String from = address;
-		String host = "localhost";
+    public void sendMail(String address, int port) throws IOException {
+        String to = address;
+        String from = address;
+        String host = "localhost";
 
-		Properties properties = System.getProperties();
-		properties.setProperty("mail.smtp.host", host);
-		properties.setProperty("mail.smtp.port",  "" + port);
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.smtp.port",  "" + port);
 
-		System.out.println("Connecting to SMTP host: " + properties.getProperty("mail.smtp.host") + " on port "
-				+ properties.getProperty("mail.smtp.port"));
+        System.out.println("Connecting to SMTP host: " + properties.getProperty("mail.smtp.host") + " on port "
+                + properties.getProperty("mail.smtp.port"));
 
-		Session session = Session.getInstance(properties);
+        Session session = Session.getInstance(properties);
 
-		try {
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(from));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-			message.setSubject("test");
-			message.setText("test mail");
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("test");
+            message.setText("test mail");
 
-			Transport.send(message);
+            Transport.send(message);
 
-		} catch (MessagingException mex) {
-			mex.printStackTrace();
-		}
-	}
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
 
 
 }
